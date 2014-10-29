2351951ad26b6a1f88d4b42f3092b32c8a61de06
abarabash <anphenom@gmail.com>
2014-10-29 19:34:14 +0200

    update tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,13 +1,19 @@
 var phantomcss = require('../node_modules/phantomcss/phantomcss.js');
 
 phantomcss.init({
-    screenshotRoot: './test/screenshots',
-    failedComparisonsRoot: './test/fail-screenshots'
+    screenshotRoot: './tests/screenshots',
+    failedComparisonsRoot: './tests/fail-screenshots',
+    libraryRoot: './node_modules/phantomcss/',
 });
 
 
 casper.start();
 casper.viewport(1024, 1024);
+
+casper.thenOpen('./documentation/templates/default/index.html')
+      .then(function () {
+          phantomcss.screenshot('.nav', 'test');
+      });
 
 casper.then(function () {
     phantomcss.compareAll();
